f5859c80d62357a2d418395e0aaafd9620d5f858
calvinvinnn <calvinvinnn@users.noreply.github.com>
2020-03-29 17:47:33 -0700

    Change reading file using BufferedReader

--- src/main/java/ca/cmpt213/as5/model/ReadCSVFile.java
+++ src/main/java/ca/cmpt213/as5/model/ReadCSVFile.java
@@ -1,8 +1,9 @@
 package ca.cmpt213.as5.model;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -10,21 +11,23 @@ import java.util.Scanner;
 
 public class ReadCSVFile {
     public void readCSV(){
-        List<String> lines;
-        {
-            try {
-                lines = Files.readAllLines(Paths.get("data\\small_data.csv"));
-                for(String line:lines){
-                    String[] result = line.split(",");
-                    for(String s : result){
-                        System.out.print(s + " ");
-                        //System.out.println();
-                    }
-                    System.out.println();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        BufferedReader objReader;
+        String strCurrentLine;
+        final String smallDataCSV = "data\\small_data.csv";
+        final String courseData2018CSV = "data\\course_data_2018.csv";
+        try {
+            objReader = new BufferedReader(new FileReader(courseData2018CSV));
+            while ((strCurrentLine = objReader.readLine()) != null) {
+                /*
+                    Testing
+                    Replace with copying to actual class
+                 */
+                System.out.println(strCurrentLine.replace(" ",""));
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
