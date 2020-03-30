68c6d1e8be2c9b939f30463b1ce08d7b0bb7c472
Seno Adhi <44691432+senoam@users.noreply.github.com>
2020-03-29 20:03:36 -0700

    Add web application

--- src/main/java/ca/cmpt213/as5/controllers/CourseController.java
+++ src/main/java/ca/cmpt213/as5/controllers/CourseController.java
@@ -5,6 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
+    String appName;
+
+    @GetMapping("/api/about")
+    public String about() {
+        String appName = "CoursePlanner";
+        return appName;
+    }
+
 
     @GetMapping("/dump-model")
     public void dumpModel() {
